6507dc2649980cdbafa37388c119a842e2204228
Jeanne Boutry <jeanne.boutry@hotmail.fr>
2022-10-25 15:18:56 +0200

    Projet terminé

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -131,9 +131,9 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    // bills.forEach(bill => {
-    // $(`#open-bill${bill.id}`).off('click');
-    // })
+    bills.forEach(bill => {
+    $(`#open-bill${bill.id}`).off('click');
+    })
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
     if (this.counter % 2 === 0) {
